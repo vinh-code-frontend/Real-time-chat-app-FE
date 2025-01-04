8f7bfd9f83737a690495cd7b4cd553dccfbe5981
Nguyen Le Vinh <79951174+vinh-code-frontend@users.noreply.github.com>
2025-01-05 00:56:13 +0700

    [RTC] Update deployed api url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://real-time-chat-app-be.onrender.com/api",
   withCredentials: true,
 })
