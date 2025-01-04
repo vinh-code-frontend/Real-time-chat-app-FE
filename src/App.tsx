08dbe5e11c9b5a7453d9815d121323c57b8a60bc
Nguyen Le Vinh <vikvenus199x@gmail.com>
2025-01-05 00:46:00 +0700

    [RTC] remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
-import { memo, useEffect } from "react";
+import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 import { useThemeStore } from "./store/useThemeStore";
